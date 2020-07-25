20ce19c1adda75c67ec4320ef17a0e6abbd81667
cvasqxz <cvxz@pm.me>
2020-07-25 18:20:35 -0400

    casi casi, leyendo cada hash calculado

--- scrypt.c
+++ scrypt.c
@@ -808,7 +808,23 @@ int scanhash_scryptCHA(int thr_id, uint32_t *pdata,
 		else
 #endif
 		scrypt_1024_1_1_256(data, hash, midstate, scratchbuf, N);
-		// ACA HAY QUE HACER EL SHA256 DEL HASH DE SCRYPT
+		uint32_t hash_be[8], newhash_be[8];
+		char hash_str[64];
+
+		for (int x = 0; x < throughput; x++) {
+			for (int y = 0; y < 8; y++) {
+				hash_be[y] = hash[y + x*8];
+			}
+
+			bin2hex(hash_str, (unsigned char *)hash_be, 32);
+			applog(LOG_DEBUG, "DEBUG(%d): %s", x, hash_str);
+
+			// ACA HAY QUE HACER EL SHA256 DE hash_be Y DEJARLO EN newhash_be
+			// ESTOY HACIENDO UN DEBUG TERRIBLE FEO XD
+
+			bin2hex(hash_str, (unsigned char *)newhash_be, 32);
+			applog(LOG_DEBUG, "DEBUG(%d): %s", x, hash_str);
+		}
 
 		for (i = 0; i < throughput; i++) {
 			if (hash[i * 8 + 7] <= Htarg && fulltest(hash + i * 8, ptarget)) {
